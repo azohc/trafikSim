a08826f3a782548757070c904c34aef72869c4db
jaman <jaman@LAPTOP-DM695PS6>
2018-03-18 17:02:33 +0100

    todo ok papasitooo

--- trafikSim/src/model/MostCrowdedJunction.java
+++ trafikSim/src/model/MostCrowdedJunction.java
@@ -14,18 +14,21 @@ public class MostCrowdedJunction extends JunctionWithTimeSlice {
 	protected void switchLights(int pos) {
 		
 		int indexMostVehicles = 0;
-		int mostVehicles = 0;
+		int mostVehicles = - 1;
 		
 		if(pos != -1) {
 			if(((IncRoadWithTimeSlice) _incRoads.get(pos)).getUsedTimeUnits() == ((IncRoadWithTimeSlice) _incRoads.get(pos)).getTimeSlice()){
 				_incRoads.get(pos).setGreen(false); // set current green light to red
 			
-				for(int i = 0; i < _incRoads.size(); i++)  // find road with most vehicles
-					if(mostVehicles < _incRoads.get(i)._queue.size()) { 
-						mostVehicles = _incRoads.get(i)._queue.size();
-						indexMostVehicles = i;
+				if(_incRoads.size() > 1) {
+
+					for(int i = 0; i < _incRoads.size(); i++) {  // find road with most vehicles
+						if(mostVehicles < _incRoads.get(i)._queue.size() && i != pos) { 
+							mostVehicles = _incRoads.get(i)._queue.size();
+							indexMostVehicles = i;
+						}	
 					}
-				
+				}
 				_incRoads.get(indexMostVehicles).setGreen(true); // set the road with most vehicles to green
 				((IncRoadWithTimeSlice) _incRoads.get(indexMostVehicles)).setTimeSlice(Math.max(mostVehicles / 2, 1 ));
 				((IncRoadWithTimeSlice) _incRoads.get(indexMostVehicles)).setUsedTimeUnits(0);	
